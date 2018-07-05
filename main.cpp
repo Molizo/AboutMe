684cfdaff8e7502dbb922cb3e55f5683d91c57a9
Molizo <visoiu.mihnea@live.com>
2018-07-05 16:36:13 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nume Elev:Visoiu Mihnea-Theodor" << endl;
+    cout << "Clasa:5B Diriginte:Cerchez Iuliana-Nicoleta" <<endl;
+    cout << "Scoala:ICHB(The International Computer High School of Bucharest)" <<endl;
+    return 0;
+}
